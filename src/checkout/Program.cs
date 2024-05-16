1f2c80dd9e5b419abd278cee1c2542fb530f20ba
Bart Jansen <bajansen@microsoft.com>
2024-05-16 14:57:56 +0200

    split checkout into Models and Services

--- src/checkout/Program.cs
+++ src/checkout/Program.cs
@@ -1,17 +1,22 @@
-using System;
 using Dapr.Client;
-using System.Text.Json.Serialization;
+using System;
 using System.Threading.Tasks;
 
-for (int i = 1; i <= 10; i++) {
-    var order = new Order(i);
-    using var client = new DaprClientBuilder().Build();
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        const string pubsubName = "orderpubsub";
+        const string topicName = "orders";
 
-    // Publish an event/message using Dapr PubSub
-    await client.PublishEventAsync("orderpubsub", "orders", order);
-    Console.WriteLine("Published data: " + order);
+        using var client = new DaprClientBuilder().Build();
+        var orderPublisher = new OrderPublisher(client);
 
-    await Task.Delay(TimeSpan.FromSeconds(1));
+        for (int i = 1; i <= 10; i++)
+        {
+            var order = new Order(i);
+            await orderPublisher.PublishOrderAsync(order, pubsubName, topicName);
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
 }
-
-public record Order([property: JsonPropertyName("orderId")] int OrderId);
